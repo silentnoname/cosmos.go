18b2d71c4728eef77431c13c3069474f12892b66
silentnoname <635397523pl@gamil.com>
2022-04-24 16:09:47 +0800

    add GenerateMnemonicandAddress

--- key/generatekey.go
+++ key/generatekey.go
@@ -1,1 +1,80 @@
 package key
+
+import (
+	"errors"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	"github.com/cosmos/cosmos-sdk/crypto/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/bech32"
+	bip39 "github.com/cosmos/go-bip39"
+)
+
+// PrivKey - wrapper to expose interface
+type PrivKey = types.PrivKey
+
+// CreateMnemonic - create new mnemonic
+func CreateMnemonic() (string, error) {
+	// Default number of words (24): This generates a mnemonic directly from the
+	// number of words by reading system entropy.
+	entropy, err := bip39.NewEntropy(256)
+	if err != nil {
+		return "", err
+	}
+
+	return bip39.NewMnemonic(entropy)
+}
+
+// CreateHDPath returns BIP 44 object from account and index parameters.
+func CreateHDPath(account uint32, cointype uint32, index uint32) string {
+	return hd.CreateHDPath(cointype, account, index).String()
+}
+
+// DerivePrivKeyBz - derive private key bytes
+func DerivePrivKeyBz(mnemonic string, hdPath string) ([]byte, error) {
+	if !bip39.IsMnemonicValid(mnemonic) {
+		return nil, errors.New("invalid mnemonic")
+	}
+
+	algo, err := keyring.NewSigningAlgoFromString(string(hd.Secp256k1Type), keyring.SigningAlgoList{hd.Secp256k1})
+	if err != nil {
+		return nil, err
+	}
+
+	// create master key and derive first key for keyring
+	return algo.Derive()(mnemonic, "", hdPath)
+}
+
+// PrivKeyGen is the default PrivKeyGen function in the keybase.
+// For now, it only supports Secp256k1
+func PrivKeyGen(bz []byte) (types.PrivKey, error) {
+	algo, err := keyring.NewSigningAlgoFromString(string(hd.Secp256k1Type), keyring.SigningAlgoList{hd.Secp256k1})
+	if err != nil {
+		return nil, err
+	}
+
+	return algo.Generate()(bz), nil
+}
+
+// default cointype for most of chains:118 ,prefix for cosmos:cosmos
+// The address will be same as the default address in keplr
+func GenerateMnemonicandAddress(cointype uint32, prefix string) (mnemonic string, address string, error error) {
+	mnemonic, err := CreateMnemonic()
+	if err != nil {
+		return "", "", err
+	}
+	privKeyBz, err := DerivePrivKeyBz(mnemonic, CreateHDPath(0, cointype, 0))
+	if err != nil {
+		return "", "", err
+	}
+	privKey, err := PrivKeyGen(privKeyBz)
+	if err != nil {
+		return "", "", err
+	}
+	accAddress := sdk.AccAddress(privKey.PubKey().Address().Bytes())
+	bech32Addr, err := bech32.ConvertAndEncode(prefix, accAddress.Bytes())
+	if err != nil {
+		return "", "", err
+	}
+	return mnemonic, bech32Addr, nil
+}
--- key/generatekey_test.go
+++ key/generatekey_test.go
@@ -1,1 +1,26 @@
 package key
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_CreateMnemonic(t *testing.T) {
+	_, err := CreateMnemonic()
+	assert.NoError(t, err)
+}
+
+func Test_DrivePrivKey(t *testing.T) {
+	mnemonic, err := CreateMnemonic()
+	assert.NoError(t, err)
+
+	// Only Secp256k1 is supported
+	_, err = DerivePrivKeyBz(mnemonic, CreateHDPath(1, 118, 1))
+	assert.NoError(t, err)
+}
+
+func Test_GenerateMnemonicandAddress(t *testing.T) {
+	_, _, err := GenerateMnemonicandAddress(118, "cosmos")
+	assert.NoError(t, err)
+}
